914c7e018a3cb504f06511887de8198b1ebab098
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-21 17:14:57 -0600

    finished form

--- src/main.js
+++ src/main.js
@@ -4,15 +4,17 @@ $(document).on('ready', function() {
 
     validateFirstName();
     validateLastName();
+    validateEmail();
   });
 });
 
 function hideCallout() {
-  $('.vaildCallout').css('display', 'none');
+  $('small').css('display', 'none');
 }
-function showCallout(msg) {
-  $('.vaildCallout > h2').text(msg);
-  $('.vaildCallout').css('display', 'block');
+function showCallout(msg, selector) {
+  console.log(selector);
+  $(selector).text(msg);
+  $(selector).css('display', 'block');
   setTimeout(hideCallout, 2000)
 }
 
@@ -23,17 +25,17 @@ function validateFirstName () {
 
   if (firstName === '')
   {
-    showCallout('Enter your first name')
+    showCallout('Enter your first name', '.smallFirstName')
   }
 
   else if (firstName.length < firstNameMinLength)
   {
-    showCallout('Your first name is too short')
+    showCallout('Your first name is too short', '.smallFirstName')
   }
 
   else if (firstName[0] !== firstName[0].toUpperCase())
   {
-    showCallout('Capitalize the first letter of your first name')
+    showCallout('Capitalize the first letter of your first name', '.smallFirstName')
   }
 }
 
@@ -44,16 +46,44 @@ function validateLastName () {
 
   if (lastName === '')
   {
-    showCallout('Enter your last name');
+    showCallout('Enter your last name', '.smallLastName');
   }
 
   else if (lastName.length < lastNameMinLength)
   {
-    showCallout('Your last name is too short');
+    showCallout('Your last name is too short', '.smallLastName');
   }
 
   else if (lastName[0] !== lastName[0].toUpperCase())
   {
-    showCallout('Capitalize the last letter of your first name');
+    showCallout('Capitalize the last letter of your first name', '.smallLastName');
   }
 }
+
+function validateEmail () {
+  var email = $('#email').val();
+
+  var at = email.indexOf('@');
+  var dot = email.indexOf('.', at);
+
+  console.log('at'+at);
+  console.log('dot'+dot);
+
+  console.log(email);
+  if (!email.includes('@'))
+  {
+    showCallout('Email is missing an @', '.smallEmail');
+  }
+  else if (dot === at+1)
+  {
+    showCallout('@ is wrong', '.smallEmail');
+  }
+  else if (at === 0)
+  {
+    showCallout('nothing before @', '.smallEmail');
+  }
+}
+
+// $('input').on('blur', function () {
+// console.log('test');
+// });
